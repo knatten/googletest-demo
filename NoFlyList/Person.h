a1f0b66bfc2a852d55d05f63af6d988afac4c453
Anders Schau Knatten <anders@knatten.org>
2012-09-11 20:53:19 +0200

    Add nationality and political view

--- NoFlyList/Person.h
+++ NoFlyList/Person.h
@@ -3,9 +3,12 @@
 class Person
 {
 public:
-	Person() : terrorist(false)
-	{
-	}
+	enum nationality_t { FRENCH, AMERICAN };
+	enum political_view_t { CONSERVATIVE, LIBERAL };
+
+	Person(nationality_t nationality, political_view_t political_view) :
+	terrorist(false), nationality(nationality), political_view(political_view) {}
+
 	void markAsTerrorist()
 	{
 		terrorist = true;
@@ -14,6 +17,18 @@ public:
 	{
 		return terrorist;
 	}
+
+	nationality_t getNationality() const
+	{ 
+		return nationality; 
+	}
+	political_view_t getPoliticalView() const
+	{
+		return political_view; 
+	}
+
 private:
 	bool terrorist;
+	nationality_t nationality;
+	political_view_t political_view;
 };
