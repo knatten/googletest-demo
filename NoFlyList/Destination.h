a65f325b2bafd9d9d47978015be2003e8fb813be
Anders Schau Knatten <anders@knatten.org>
2012-09-23 14:11:37 +0200

    Add operator== to Destination

--- NoFlyList/Destination.h
+++ NoFlyList/Destination.h
@@ -7,4 +7,6 @@ struct Destination
 	Destination(std::string name, unsigned int nofHumanRights) : name(name), nofHumanRights(nofHumanRights) {}
 	const std::string name;
 	const unsigned int nofHumanRights;
+
+	bool operator==(const Destination& rhs) const { return name == rhs.name; }
 };
