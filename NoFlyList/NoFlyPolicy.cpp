bd61808f8d1a067b307e04db230da2f581ce6fe8
Anders Schau Knatten <anders@knatten.org>
2012-09-26 22:07:46 +0200

    Add code from the live demo today

--- NoFlyList/NoFlyPolicy.cpp
+++ NoFlyList/NoFlyPolicy.cpp
@@ -1,7 +1,28 @@
 #include "NoFlyPolicy.h"
 
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
 
 bool NoFlyPolicy::canFly(const Person& person)
 {
-	return false;
+	if (person.isTerrorist())
+		return false;
+	if (person.getNationality() == Person::FRENCH && person.getPoliticalView() == Person::LIBERAL)
+		return false;
+
+	vector<string> forbidden;
+	forbidden.push_back("bomb");
+	forbidden.push_back("osama");
+	forbidden.push_back("obama");
+
+	auto twitter = person.getTweets();
+	for(auto tweet = twitter.begin(); tweet != twitter.end(); ++tweet)
+		for(auto f = forbidden.begin(); f != forbidden.end(); ++f)
+			if (tweet->find(*f) != string::npos)
+				return false;
+
+	return true;
 }
---
+++ NoFlyListTest/NoFlyPolicyTest.cpp
@@ -0,0 +1,95 @@
+#include "..\NoFlyList\NoFlyPolicy.h"
+#include "..\NoFlyList\Person.h"
+
+#include <gtest\gtest.h>
+
+#include <memory>
+#include <string>
+
+using std::make_shared;
+using std::string;
+
+class NoFlyPolicyTest : public ::testing::Test
+{
+public:
+	NoFlyPolicyTest() : person(Person::AMERICAN, Person::CONSERVATIVE) {};
+protected:
+	NoFlyPolicy policy;
+	Person person;
+};
+
+TEST_F(NoFlyPolicyTest, niceGuysCanFly)
+{
+	ASSERT_TRUE(policy.canFly(person));
+}
+
+TEST_F(NoFlyPolicyTest, terroristsCannotFly)
+{
+	person.markAsTerrorist();
+	ASSERT_FALSE(policy.canFly(person));
+}
+
+TEST_F(NoFlyPolicyTest, frenchLiberalsCannotFly)
+{
+	Person frenchLiberal(Person::FRENCH, Person::LIBERAL);
+	ASSERT_FALSE(policy.canFly(frenchLiberal));
+}
+
+TEST_F(NoFlyPolicyTest, personWhoDidntTweetCanFly)
+{
+	auto twitter = make_shared<Twitter>();
+	person.connectTwitterAccount(twitter);
+	ASSERT_TRUE(policy.canFly(person));
+}
+
+TEST_F(NoFlyPolicyTest, personWhoTweetedSometingInnocentCanFly)
+{
+	auto twitter = make_shared<Twitter>();
+	twitter->tweet("OMG SUPERBOWL IS NICE GOD BLESS BUD!!!!11");
+	person.connectTwitterAccount(twitter);
+	ASSERT_TRUE(policy.canFly(person));
+}
+
+class NoFlyPolicyTestWithTweets : public NoFlyPolicyTest, public ::testing::WithParamInterface<string> {};
+
+TEST_P(NoFlyPolicyTestWithTweets, personWhoTweetedSometingBadCannotFly)
+{
+	auto twitter = make_shared<Twitter>();
+	twitter->tweet(GetParam());
+	person.connectTwitterAccount(twitter);
+	ASSERT_FALSE(policy.canFly(person));
+}
+
+INSTANTIATE_TEST_CASE_P(_, NoFlyPolicyTestWithTweets, ::testing::Values("bomb", "osama", "obama"));
+
+::testing::AssertionResult isBadPlace(const Destination& d)
+{
+	if (d.nofHumanRights == 0)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << d << " has " << d.nofHumanRights << " human rights, expected 0";
+}
+
+TEST_F(NoFlyPolicyTest, terroristsGoToABadPlace)
+{
+	Destination d = policy.terroristDestination();
+	ASSERT_TRUE(isBadPlace(d));
+}
+
+void assertIsBadPlace(const Destination& d)
+{
+	ASSERT_EQ(0, d.nofHumanRights);
+}
+
+TEST_F(NoFlyPolicyTest, terroristsGoToABadPlace2)
+{
+	Destination d = policy.terroristDestination();
+	assertIsBadPlace(d);
+}
+
+TEST_F(NoFlyPolicyTest, terroristsGoToGitmo)
+{
+	Destination d = policy.terroristDestination();
+	Destination gitmo("Guantanamo", 1);
+	ASSERT_EQ(gitmo, d);
+}
